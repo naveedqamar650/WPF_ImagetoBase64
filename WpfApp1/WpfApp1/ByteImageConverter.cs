321062e3374fb5fc6728b62259d23d29fd0085f7
M. Naveed Qamar <naveedqamar6500@gmail.com>
2022-07-31 22:58:58 +0500

    ImageToBase64

---
+++ WpfApp1/WpfApp1/ByteImageConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    public class ByteImageConverter
+    {
+        public static ImageSource ByteToImage(byte[] imageData)
+        {
+            BitmapImage biImg = new BitmapImage();
+            MemoryStream ms = new MemoryStream(imageData);
+            biImg.BeginInit();
+            biImg.StreamSource = ms;
+            biImg.EndInit();
+
+            ImageSource imgSrc = biImg as ImageSource;
+
+            return imgSrc;
+        }
+
+        public static string ImageToByte(FileStream fs)
+        {
+            byte[] imgBytes = new byte[fs.Length];
+            fs.Read(imgBytes, 0, Convert.ToInt32(fs.Length));
+            string encodeData = Convert.ToBase64String(imgBytes, Base64FormattingOptions.InsertLineBreaks);
+
+            return encodeData;
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnChargerImage.Click += new RoutedEventHandler(btnChargerImage_Click);
+            btnOuvrirImage.Click += new RoutedEventHandler(btnOuvrirImage_Click);
+        }
+        void btnChargerImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbBytes.Text != null && tbBytes.Text.Length > 0)
+            {
+                // Convert TextBox in a Byte array
+                byte[] imgStr = Convert.FromBase64String(tbBytes.Text);
+                imgImage.Source = ByteImageConverter.ByteToImage(imgStr);
+            }
+        }
+
+        void btnOuvrirImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofg = new OpenFileDialog();
+            ofg.Title = "Select an image";
+            ofg.Filter = "Images (.jpg, .jpeg, .png, .gif, .bmp)|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+            ofg.Multiselect = false;
+
+            if (ofg.ShowDialog() == true)
+            {
+                if (ofg.FileName != null && ofg.FileName.Length > 0)
+                {
+                    ofg.OpenFile();
+                    FileStream fs = new FileStream(ofg.FileName, FileMode.Open, FileAccess.Read);
+                    tbBytes.Text = ByteImageConverter.ImageToByte(fs);
+
+                    byte[] imgStr = Convert.FromBase64String(tbBytes.Text);
+                    imgImage.Source = ByteImageConverter.ByteToImage(imgStr);
+                }
+            }
+        }
+    }
+}
